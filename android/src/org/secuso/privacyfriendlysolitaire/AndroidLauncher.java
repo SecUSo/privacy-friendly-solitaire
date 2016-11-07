15b0e10e95b595118647d2acda6c8e5922368a03
I. Dix <isabella.dix@t-online.de>
2016-11-07 16:44:46 +0100

    Test 1

---
+++ android/src/org/secuso/privacyfriendlysolitaire/AndroidLauncher.java
@@ -0,0 +1,44 @@
+package org.secuso.privacyfriendlysolitaire;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.surfaceview.GLSurfaceView20;
+
+import org.secuso.privacyfriendlysolitaire.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.outer_layout);
+
+		// just for demo-reasons
+		final Button testButton = (Button) findViewById(R.id.test_button);
+		testButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast toast = Toast.makeText(getApplicationContext(), "Reaktion", Toast.LENGTH_SHORT);
+				toast.setGravity(Gravity.BOTTOM, 0, 0);
+				toast.show();
+			}
+		});
+
+		GLSurfaceView20 gameView =
+				(GLSurfaceView20) initializeForView(new Game(), new AndroidApplicationConfiguration());
+
+		LinearLayout outerLayout = (LinearLayout) findViewById(R.id.outer);
+		outerLayout.addView(gameView);
+
+		// skip basic_layout and show game on full screen
+//		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+//		initialize(new TestGame(), config);
+	}
+}
