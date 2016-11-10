ccf0f4d6cff229c85c65234b6b06365d9e5fdbcf
I. Dix <isabella.dix@t-online.de>
2016-11-10 11:38:54 +0100

    more corporate design from example app, added splash screen as in example app, add main activity from example app

---
+++ android/src/org/secuso/privacyfriendlysolitaire/SplashActivity.java
@@ -0,0 +1,17 @@
+package org.secuso.privacyfriendlysolitaire;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+        SplashActivity.this.startActivity(mainIntent);
+        SplashActivity.this.finish();
+    }
+}
