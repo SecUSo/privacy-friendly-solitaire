1e82ad9516fac68baf26390bec637010907575b7
meric-doga <doa_s@live.de>
2016-11-27 21:14:17 +0100

    Help and Setting Activity angelegt

--- android/src/org/secuso/privacyfriendlysolitaire/Activities/MainActivity.java
+++ android/src/org/secuso/privacyfriendlysolitaire/Activities/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
 
         config = new Config(getApplicationContext());
         // the method will
