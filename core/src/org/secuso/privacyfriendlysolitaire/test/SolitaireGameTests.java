8dc85f7b401b5c2ebe940eae6323f351c1062222
I. Dix <isabella.dix@t-online.de>
2017-01-19 11:44:54 +0100

    fixed errors in tests

--- core/src/org/secuso/privacyfriendlysolitaire/test/SolitaireGameTests.java
+++ core/src/org/secuso/privacyfriendlysolitaire/test/SolitaireGameTests.java
@@ -7,6 +7,8 @@ import org.secuso.privacyfriendlysolitaire.generator.GeneratorSolitaireInstance;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.secuso.privacyfriendlysolitaire.game.Constants.MODE_ONE_CARD_DEALT;
+import static org.secuso.privacyfriendlysolitaire.game.Constants.MODE_STANDARD;
 
 /**
  * @author: M. Fischer
@@ -16,7 +18,7 @@ public class SolitaireGameTests {
 
     @Test
     public void cloneTest() {
-        SolitaireGame s1 = GeneratorSolitaireInstance.generateInstance(1);
+        SolitaireGame s1 = GeneratorSolitaireInstance.generateInstance(MODE_ONE_CARD_DEALT, MODE_STANDARD);
         SolitaireGame s2 = s1.clone();
         assertEquals(s1.getDeckWaste().getNumTurnOver(), s2.getDeckWaste().getNumTurnOver());
         s2.getDeckWaste().setNumTurnOver(3);
