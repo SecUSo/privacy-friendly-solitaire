730f93f9e0db8e268473c0b720d5b95f688f6211
I. Dix <isabella.dix@t-online.de>
2017-02-26 17:47:57 +0100

    added icons and changed everything to conform with the PFA guide (icon-wise ^^)

--- android/src/org/secuso/privacyfriendlysolitaire/Activities/MenuActivity.java
+++ android/src/org/secuso/privacyfriendlysolitaire/Activities/MenuActivity.java
@@ -177,36 +177,5 @@ public class MenuActivity extends BaseActivity {
             return rootView;
         }
     }
-
-
-    public static class WelcomeDialog extends DialogFragment {
-
-        // method onAttach removed, was deprecated
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-            LayoutInflater i = getActivity().getLayoutInflater();
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setView(i.inflate(R.layout.welcome_dialog, null));
-            builder.setIcon(R.mipmap.icon);
-            builder.setTitle(getActivity().getString(R.string.welcome));
-            builder.setPositiveButton(getActivity().getString(R.string.okay), null);
-            builder.setNegativeButton(getActivity().getString(R.string.viewhelp), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    ((MainActivity) getActivity()).goToNavigationItem(R.id.nav_help);
-                }
-            });
-
-            return builder.create();
-        }
-    }
-    public void onClick(View view) {
-        switch (view.getId()) {
-            // do something with all these buttons?
-            default:
-        }
-    }
 }
 
